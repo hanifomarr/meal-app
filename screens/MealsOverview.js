76cef2dc52c5279cddc2dea21007594bd1ce7458
Hanif Omar <hanif.omarr@gmail.com>
2023-07-11 10:02:43 +0800

    Display mealItem

--- screens/MealsOverview.js
+++ screens/MealsOverview.js
@@ -1,11 +1,25 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import React from "react";
+import { MEALS } from "../data/data";
+import MealItem from "../components/MealItem";
 
 const MealsOverview = ({ route }) => {
   const catID = route.params.categoryID;
+  const displayMealItem = MEALS.filter(
+    (itemdata) => itemdata.categoryIds.indexOf(catID) >= 0
+  );
+
+  function renderMealList(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
   return (
     <View>
-      <Text>MealsOverview{catID}</Text>
+      <FlatList
+        data={displayMealItem}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealList}
+      />
     </View>
   );
 };
